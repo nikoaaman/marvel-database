7281ede04b173eb9c1b162073cf21aad49699089
nikoaaman <nikolasaaman@gmail.com>
2023-07-24 19:01:17 -0300

    included an api request to a character list

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,12 @@
+const Card=()=>{
+    return(
+        <>
+            <div className="card">
+                <h2>1</h2>
+                <img src="" alt="" />
+            </div>
+        </>
+    )
+}
+
+export default Card;
---
+++ src/Components/Hash/Hash.jsx
@@ -0,0 +1,29 @@
+import md5 from "md5";
+import axios from "axios";
+
+
+const getCurrentTimestamp= Date.now();
+const privateKey="e2b0abb61a978417651ee7321ecc7eef90adde5b";
+const publicKey="c655cb30c70af2339dad08687db22892"
+const hash = md5(getCurrentTimestamp+privateKey+publicKey)
+
+const getCharacters = async()=>{
+    try {
+        const characters= await axios.get('http://gateway.marvel.com/v1/public/characters',{
+            params: {
+                ts : getCurrentTimestamp,
+                apikey : publicKey,
+                hash : hash 
+            }
+        })
+    console.log(characters.data.data.results[13].name)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+getCharacters();
+
+
+export default getCharacters;
